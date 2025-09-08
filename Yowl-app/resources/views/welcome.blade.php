d17e7dd565018def87ff476139636530bd1c7139
Madjalia <madjalia.ouattara@epitech.eu>
2025-09-08 04:35:47 +0000

    [EDIT] : Home-page

--- Yowl-app/resources/views/welcome.blade.php
+++ Yowl-app/resources/views/welcome.blade.php
@@ -90,7 +90,7 @@
             <input type="text" id="title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" autocomplete="off" />
         </div>
         <div class="mb-5">
-            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">URL</label>
             <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" autocomplete="off" required />
         </div>
         </form>
