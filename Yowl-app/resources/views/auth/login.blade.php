d6da2501dd0d8c754944d0163b0468f60bb5d626
Madjalia <madjalia.ouattara@epitech.eu>
2025-09-10 20:00:50 +0000

    [EDIT] : redirection

--- Yowl-app/resources/views/auth/login.blade.php
+++ Yowl-app/resources/views/auth/login.blade.php
@@ -68,11 +68,6 @@
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
-<<<<<<< HEAD
-
-=======
-<!-- @resources/views/auth/register.blade.php -->
->>>>>>> origin/test
                 <div class="flex items-center justify-end mt-4">
                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
                     {{ __("Don't have an account?") }}
