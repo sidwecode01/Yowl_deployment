24157e938c40ae6e5534ccd269e62baaaa024bc4
wecode <sidoine.nango@epitech.eu>
2025-09-10 21:09:50 +0000

    [FIX]: search bare with nothing found in the BD

--- Yowl-app/resources/views/searchPost.blade.php
+++ Yowl-app/resources/views/searchPost.blade.php
@@ -157,13 +157,13 @@
 </div>
 
 @if (count($posts) == 0)
-  <section class="bg-white dark:bg-gray-900">
+  <section class="bg-gray-100 dark:bg-gray-900  shadow-xl/20 rounded-lg ">
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div class="mx-auto max-w-screen-sm text-center">
             <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">404 😥</h1>
             <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">Something's missing.</p>
             <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">Sorry, we can't find post for : <span class="text-3xl text-gray-900">{{ $search }}</span> </p>
-            <a href="#" class="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4">Back to Homepage</a>
+            <a href="/Home" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Back to Homepage</a>
         </div>   
     </div>
 </section>
